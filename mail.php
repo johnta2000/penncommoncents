04a38cf5577a4bb253f14420c254518a68befe20
johntaaa <jta2000@sas.upenn.edu>
2019-08-21 16:41:30 -0400

    form update and contact page update

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$formcontent="From: $name \n Message: $message";
+	$recipient = "jta2000@sas.upenn.edu";
+	$subject = "Test";
+	$mailheader = "From: $email \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	echo "Thank You!";
+?>
